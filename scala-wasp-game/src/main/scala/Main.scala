d2862e9dfa11d56532d734b94b41e1c286f5b7d3
Matt <designs.sleephawk@gmail.com>
2026-03-03 10:41:38 +0000

    added case dialogue for shots

--- scala-wasp-game/src/main/scala/Main.scala
+++ scala-wasp-game/src/main/scala/Main.scala
@@ -13,7 +13,7 @@ import scala.io.StdIn.readLine
       gameName = readLine()
       
       printWasp()
-      println("Welcome " + name)
+      println("Welcome " + gameName)
       println("You stand before a wasp's nest, and it's up to you to kill every last wasp.")
       println("")
       println("Problem is you're using a peashooter, but don't worry! You have the needlepoint accuracy of a greek deity.")
@@ -22,7 +22,7 @@ import scala.io.StdIn.readLine
 
       if (acc <= 0){
       println("CONTROLS:")
-      println("fire': shoots a wasp")
+      println("'fire': shoots a wasp")
       println("'auto': autofire through the game; see how many shots it takes")
       println("'quit': end the game....already...")}
         val command = readLine()
@@ -69,6 +69,21 @@ import scala.io.StdIn.readLine
         cleanedNest.foreach(println)
         val n: Int = acc + 1
         println("you've used this many bullets: " + n)
+        n match
+          case 20 => println("Wooo feel the rush! Carry on:")
+          case 30 => println("30 wow you're a real ol school cowboy type eh")
+          case 50 => println("I bet your hands are cramping up at this stage surely?")
+          case 51 => println("How many bullets do you even have left? What kind of gun is that?")
+          case 52 => println("Damn")
+          case 53 => println("DAMN")
+          case 54 => println("Still going huh?")
+          case 55 => println("I guess I'm not helping")
+          case 60 => println("WHAT IS HAPPENING 60 BULLETS LIKE JUST SHOOT THE QUEEN")
+          case 70 => println("Isn't the queen like obvious or whatever? I know it's 'random' but it can't be THAT random")
+          case 80 => println("I'm not sure this as ever happened")
+          case 86 => println("87 shots is literally the worst you could do. Do you enjoy killing wasps? How did you milk it this long? You deserve some kind of messed up reward.")
+          case _ => println("Enter a command")
+      
         runGame(n, cleanedNest, false, gameName)
 
       }} else if (command == "auto"){
